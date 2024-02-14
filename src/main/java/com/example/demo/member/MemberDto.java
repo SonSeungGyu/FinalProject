cdb3a49a7e13fb1f375c19eee09cda7129d20142
HanTaeGyu <to3323@naver.com>
2024-02-14 10:05:14 +0900

    memberImpormatin2

--- src/main/java/com/example/demo/member/MemberDto.java
+++ src/main/java/com/example/demo/member/MemberDto.java
@@ -4,12 +4,16 @@ import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
 @Builder
 public class MemberDto {
 
--- src/main/java/com/example/demo/member/MemberEntity.java
+++ src/main/java/com/example/demo/member/MemberEntity.java
@@ -8,12 +8,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
 @Builder
 @Entity
 @Table(name="tbl_member")
