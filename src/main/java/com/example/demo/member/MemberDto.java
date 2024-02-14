69579cfa21831c43dd5470ef8a599bd136357eb9
HanTaeGyu <to3323@naver.com>
2024-02-14 10:01:53 +0900

    MemberInformation

---
+++ src/main/java/com/example/demo/member/MemberDto.java
@@ -0,0 +1,38 @@
+package com.example.demo.member;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class MemberDto {
+
+	String memberId; // 회원 아이디(pk)
+
+	String role; // 사용자 등급 추가 (사용자:ROLE_USER, 관리자:ROLE_ADMIN)
+	
+	int memberNo; //회원 넘버
+	
+	String memberPassword; // 회원 비밀번호
+
+	String memberName; // 회원 이름
+
+	String memberAddress; // 회원 주소
+
+	String memberEmail; // 회원 이메일
+
+	String memberNumber; // 회원 전화번호
+
+	String memberBirthDay; // 회원 생년월일 
+
+	LocalDateTime regDate; // 회원이 최초로 만들어진 시간
+	
+	LocalDateTime modDate; // 회원 정보 수정된 시간
+	
+}
---
+++ src/main/java/com/example/demo/member/MemberEntity.java
@@ -0,0 +1,49 @@
+package com.example.demo.member;
+
+import com.example.demo.allBaseEtity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name="tbl_member")
+public class MemberEntity extends BaseEntity {
+
+	@Id
+	@Column(length = 50)
+	String memberId; // 회원 아이디(pk값)
+	
+	int memberNo; // 회원 넘버
+	
+	@Column(length = 50, nullable = false)
+	String memberPassword; // 회원 비밀번호
+	
+	@Column(length = 50, nullable = false)
+	String memberName; // 회원 이름
+	
+	@Column(length = 50, nullable = false)
+	String memberAddress; // 회원 주소
+	
+	@Column(length = 50, nullable = false)
+	String memberEmail; // 회원 이메일
+	
+	@Column(length = 50, nullable = false)
+	String memberNumber; // 회원 전화번호
+	
+	@Column(length = 50, nullable = false)
+	String memberBirthDay; // 회원 생년월일
+	
+	
+	
+	
+}
