53978d5a7bcb55e34b0395a21ef01c5eb030ad14
sukjun1 <sukjun6118@naver.com>
2024-02-15 12:34:25 +0900

    init

--- src/main/java/com/example/demo/notice/dto/NoticeDto.java
+++ src/main/java/com/example/demo/notice/dto/NoticeDto.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 import com.example.demo.member.entitly.MemberEntity;
 import com.example.demo.notice.entity.NoticeEntity;
-import com.example.demo.trainer.entity.TrainerEntity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- src/main/java/com/example/demo/trainer/repository/TrainerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.trainer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.trainer.entity.TrainerEntity;
-
-public interface TrainerRepository extends JpaRepository<TrainerEntity, String>{
-
-}
