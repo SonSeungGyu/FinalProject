e1467ecacc649ddc1a4347ff467402eb1342e370
sukjun1 <sukjun6118@naver.com>
2024-02-15 09:22:34 +0900

    controller 추가

--- src/main/java/com/example/demo/notice/controller/NoticeController.java
+++ src/main/java/com/example/demo/notice/controller/NoticeController.java
@@ -1,10 +1,17 @@
 package com.example.demo.notice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.notice.dto.NoticeDto;
 import com.example.demo.notice.service.NoticeService;
+
+
 
 @Controller
 @RequestMapping("/notice")
@@ -13,4 +20,9 @@ public class NoticeController {
 	@Autowired
 	NoticeService service;
 	
+	@GetMapping("/list")
+	public void list(@RequestParam(defaultValue = "0",name = "page")int page,Model model) {
+		Page<NoticeDto> list = service.getList(page);
+		model.addAttribute("list", list);
+	}
 }
