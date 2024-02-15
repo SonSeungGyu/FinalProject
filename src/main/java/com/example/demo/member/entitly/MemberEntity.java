be83225d438c8bb8cd88e25ba58b11fade28416c
HanTaeGyu <to3323@naver.com>
2024-02-15 11:43:41 +0900

    member2

--- src/main/java/com/example/demo/member/entitly/MemberEntity.java
+++ src/main/java/com/example/demo/member/entitly/MemberEntity.java
@@ -26,25 +26,25 @@ import lombok.ToString;
 public class MemberEntity extends BaseEntity {
 
 	@Id
-	@Column(length = 50)
+	@Column(length = 100)
 	String memberId; // 회원 아이디(pk값)
 
-	@Column(length = 50, nullable = false)
+	@Column(length = 400, nullable = false)
 	String memberPassword; // 회원 비밀번호
 	
 	@Column(length = 50, nullable = false)
 	String memberName; // 회원 이름
 	
-	@Column(length = 50, nullable = false)
+	@Column(length = 100, nullable = false)
 	String memberAddress; // 회원 주소
 	
-	@Column(length = 50, nullable = false)
+	@Column(length = 100, nullable = false)
 	String memberEmail; // 회원 이메일
 	
-	@Column(length = 50, nullable = false)
+	@Column(length = 100, nullable = false)
 	String memberNumber; // 회원 전화번호
 	
-	@Column(length = 50, nullable = false)
+	@Column(length = 100, nullable = false)
 	String memberBirthDay; // 회원 생년월일
 	
 	@Column(length = 100,nullable = false)
--- src/test/java/com/example/demo/MemberServiceTest/ServiceTest.java
+++ src/test/java/com/example/demo/MemberServiceTest/ServiceTest.java
@@ -1,9 +1,13 @@
 package com.example.demo.MemberServiceTest;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 
+import com.example.demo.member.dto.MemberDto;
 import com.example.demo.member.service.MemberService;
 
 @SpringBootTest
@@ -13,7 +17,37 @@ public class ServiceTest {
 	MemberService service;
 	
 	@Test
-	public void 회원조회() {}
+	public void 회원목록조회() {
+		Page<MemberDto>page = service.getList(1);
+		
+		List<MemberDto>list =page.getContent();
+		
+		for(MemberDto dto : list) {
+			System.out.println(dto);
+		}
+	}
+	
+	@Test
+	public void 회원가입() {
+		MemberDto dto = MemberDto.builder()
+				.memberId("user1")
+				.memberPassword("1234")
+				.memberName("아산백호")
+				.memberAddress("인천")
+				.memberEmail("isu@naber.com")
+				.memberNumber("010-1234-5678")
+				.memberBirthDay("2001-07-07")
+				.build();
+		boolean result = service.register(dto);
+		System.out.println(result);
+	}
+
+	
+	@Test
+	public void 회원조회() {
+		MemberDto dto = service.read("user1");
+		System.out.println(dto);
+	}
 
 	
 }
