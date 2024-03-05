26ede474391654e291367da48868d3005351cacf
Son Seung Gyu <thstmd0705@naver.com>
2024-03-05 13:20:56 +0900

    final merge2

--- src/main/java/com/example/demo/result/repository/ResultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.result.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.result.entity.ResultEntity;
-//2024-02-20 결과테이블 생성
-public interface ResultRepository extends JpaRepository<ResultEntity, Integer>{
-
-}
